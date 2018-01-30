56ee51091f1573afdd8a135294372a37e66b0ab2
Ben Hodgson <bmh43@duke.edu>
2018-01-30 16:21:26 -0500

    Created some basic abstract methods in the Screen super class

--- src/Screen/Screen.java
+++ src/Screen/Screen.java
@@ -1,13 +1,44 @@
 package Screen;
 
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 /**
  * 
  * @author Benjamin Hodgson
+ * @author Katherine Van Dyk
+ * @author Michael Acker
  * @date 1/30/18
  *
+ * The Screen class is a super class that contains some basic methods used 
+ * to generate some scene roots.
  * 
  */
 public abstract class Screen {
-
-
+	
+	protected Parent ROOT;
+	
+	/**
+	 * 
+	 * @return ROOT: The Parent node to be used in the Scene object. 
+	 */
+	public abstract Parent getRoot();
+	
+	/**
+	 * Creates a Label object bearing the desired text.
+	 * 
+	 * @param text: A String containing the text to be displayed on the Label
+	 * @return Label object to be put in the Scene root
+	 */
+	public abstract Label makeLabel(String text);
+	
+	/**
+	 * Creates a Button object bearing the desired text.
+	 * 
+	 * @param text: A String containing the text to be displayed on the Button
+	 * @return Button object to be put in the Scene root
+	 */
+	public abstract Button makeButton(String text);
+	
 }
--- src/cellsociety_team01/Driver.java
+++ src/cellsociety_team01/Driver.java
@@ -3,6 +3,8 @@ package cellsociety_team01;
 /** 
  * 
  * @author Benjamin Hodgson
+ * @author Katherine Van Dyk
+ * @author Michael Acker
  * @date 1/30/18
  *
  */
