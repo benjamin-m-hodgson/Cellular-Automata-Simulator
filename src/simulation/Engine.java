f64ae458c92cf9ad4e2b3611020d0caf880b55af
Ben Hodgson <bmh43@duke.edu>
2018-02-08 16:06:32 -0500

    Merging with master

--- src/simulation/Engine.java
+++ src/simulation/Engine.java
@@ -45,7 +45,7 @@ public class Engine {
 	private Stage PROGRAM_STAGE;
 	private Scene PROGRAM_SCENE;
 	private String SIMULATION_TYPE;
-	private String SHAPE_TYPE = "Triangle";
+	private String SHAPE_TYPE = "Rectangle";
 	private int GENERATION;
 	private boolean SIMULATING;
 	private CurrentSimulation SIMULATION;
