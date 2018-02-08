2c9150838d39417339a9f02f44c2383068e217ed
Ben Hodgson <bmh43@duke.edu>
2018-02-08 16:25:28 -0500

    Created the Refactoring Review markdown

---
+++ doc/REFACTORING_DISCUSSION_bmh43_kmv10.md
@@ -0,0 +1,9 @@
+# cellsociety
+CompSci 308 Refactoring discussion
+
+## Team 1
+* Ben Hodgson
+* Katherine Van Dyk
+
+## Duplication Refactoring
+*  
--- src/simulation/CurrentSimulation.java
+++ src/simulation/CurrentSimulation.java
@@ -43,6 +43,17 @@ public class CurrentSimulation {
 		return SIMULATION_SHAPES[x][y];
 	}
 	
+	
+	public void makeNewXML() {
+		XMLWriter writer = new XMLWriter();
+		try {
+			writer.createDoc("Fire", "hello", PROGRAM_ENGINE.currentGrid(), PROGRAM_ENGINE.currentRules());
+		} catch (TransformerConfigurationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	private void initializeShapes() {
 		Grid currentGrid = PROGRAM_ENGINE.currentGrid();
 		Cell[][] currentCells = currentGrid.getCells();
@@ -117,15 +128,4 @@ public class CurrentSimulation {
 			}
 		}
 	}
-	
-	public void makeNewXML() {
-		XMLWriter writer = new XMLWriter();
-		try {
-			writer.createDoc("Fire", "hello", PROGRAM_ENGINE.currentGrid(), PROGRAM_ENGINE.currentRules());
-		} catch (TransformerConfigurationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-
 }
