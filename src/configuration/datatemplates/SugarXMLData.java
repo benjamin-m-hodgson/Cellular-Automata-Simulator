5326ded72b403a65363d14194be81196b17acd1c
Michael Acker <mja39@duke.edu>
2018-02-11 23:56:26 -0500

    sugarscapexml finished

--- src/configuration/datatemplates/SugarXMLData.java
+++ src/configuration/datatemplates/SugarXMLData.java
@@ -3,6 +3,7 @@ package configuration.datatemplates;
 import java.util.Arrays;
 import java.util.List;
 
+import configuration.XMLParsing.CellStateGenerator;
 import simulation.grid.Grid;
 import simulation.cell.SugarCell;
 import simulation.grid.StandardGrid;
@@ -17,7 +18,12 @@ public class SugarXMLData extends XMLData {
 	private final int AGENT = 1; 
 	protected static final List<String> PARAM_DATA_FIELDS = Arrays.asList(new String[] {
 			"regenRate",
-			"regenInterval"
+			"regenInterval",
+			"sugar",
+			"maxSugar",
+			"agentSugar",
+			"agentMetabolism",
+			"agentVision"
 	});
 
 	/**
@@ -62,11 +68,20 @@ public class SugarXMLData extends XMLData {
 	public Grid getGrid(int[][] states) {
 		Grid g = new StandardGrid(this.getXSize(), this.getYSize());
 		g.setType(SUGAR);
-		int sugar = Integer.parseInt(myDataValues.get(PARAM_DATA_FIELDS.get(0)));
-		int maxSugar = Integer.parseInt(myDataValues.get(PARAM_DATA_FIELDS.get(0)));
+		String sugar = myDataValues.get(PARAM_DATA_FIELDS.get(2));
+		int[][] sugarStates = new CellStateGenerator().locationStates(sugar, getXSize(), getYSize());
+		String maxSugar = myDataValues.get(PARAM_DATA_FIELDS.get(3));
+		int[][] maxSugarStates = new CellStateGenerator().locationStates(maxSugar, getXSize(), getYSize());
+		String agentSugar = myDataValues.get(PARAM_DATA_FIELDS.get(4));
+		int[][] agentSugarStates = new CellStateGenerator().locationStates(agentSugar, getXSize(), getYSize());
+		String agentMetabolism = myDataValues.get(PARAM_DATA_FIELDS.get(5));
+		int[][] agentMetabolismStates = new CellStateGenerator().locationStates(agentMetabolism, getXSize(), getYSize());
+		String agentVision = myDataValues.get(PARAM_DATA_FIELDS.get(6));
+		int[][] agentVisionStates = new CellStateGenerator().locationStates(agentVision, getXSize(), getYSize());
+		//public SugarCell(int x, int y, int state, int sugar, int maxSugar, int agentSugar, int agentMetabolism, int agentVision)
 		for(int r= 0; r < this.getXSize(); r++) {
 			for(int c = 0; c < this.getYSize(); c++) {
-				g.addCell(new SugarCell(r, c, states[r][c], sugar, maxSugar));
+				g.addCell(new SugarCell(r, c, states[r][c], sugarStates[r][c], maxSugarStates[r][c], agentSugarStates[r][c], agentMetabolismStates[r][c], agentVisionStates[r][c]));
 			}
 		}
 		return g;
