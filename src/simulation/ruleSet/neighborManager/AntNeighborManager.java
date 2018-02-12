a2d8d73cbf33d03830e23635fc00ff69a0868e82
Michael Acker <mja39@duke.edu>
2018-02-11 20:40:58 -0500

    Tried adding xml stuff and commented some things

---
+++ src/simulation/ruleSet/neighborManager/AntNeighborManager.java
@@ -0,0 +1,19 @@
+package simulation.ruleSet.neighborManager;
+
+import simulation.cell.AntCell;
+import simulation.cell.Cell;
+import simulation.grid.Grid;
+
+public class AntNeighborManager extends NeighborManager {
+
+	@Override
+	protected Cell[] getNeighbors(Cell c, Grid g) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public AntCell[] getMaxHomeNeighbor(AntCell c) {
+		
+	}
+
+}
